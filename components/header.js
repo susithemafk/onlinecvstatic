323c8b175f04e3fd51cfd1ba3d7c236cd57d4396
susithemafk <m.sucharda@email.cz>
2023-05-24 23:42:09 +0200

    new init

---
+++ components/header.js
@@ -0,0 +1,34 @@
+const custom_header = document.querySelector('#custom_header')
+custom_header.className = "header text-light px-lg-0 px-3"
+
+// container in header
+const header_container = document.createElement('div')
+header_container.className = "container-medium mx-auto row justify-content-between"
+custom_header.appendChild(header_container)
+
+// links in container
+const homeLink = document.createElement('a')
+const projectsLink = document.createElement('a')
+
+homeLink.href = "../../index.html" 
+projectsLink.href = "../../projects.html"
+
+// text in links
+const homeLinkText = document.createElement('h2')
+const projectsLinkText = document.createElement('h2')
+
+homeLinkText.className = "py-4 fs-2"
+projectsLinkText.className = "py-4 fs-2"
+
+homeLinkText.textContent = "susithemafk"
+projectsLinkText.textContent = "projekty"
+
+
+homeLink.appendChild(homeLinkText)
+projectsLink.appendChild(projectsLinkText)
+
+header_container.appendChild(homeLink)
+header_container.appendChild(projectsLink)
+
+
+custom_header.appendChild(header_container)
---
+++ components/projects.js
@@ -0,0 +1,77 @@
+const custom_projects = document.querySelector('#custom_projects')
+const count = custom_projects.getAttribute("count")
+
+const projects = [
+    {
+        src: "../assets/projects/susithechain/hero.jpg", 
+        alt: 'susithechain', 
+        link: '/projects/susithechain.html', 
+        title: 'susithechain'
+    }, 
+    {
+        src: "../assets/projects/skatespotter/hero.webp", 
+        alt: 'skatespotter', 
+        link: '/projects/skatespotter.html', 
+        title: 'skatespotter'
+    }, 
+    {
+        src: "../assets/projects/teachablewithsimpsons/hero.webp", 
+        alt: 'teachablewithsimpsons', 
+        link: '/projects/teachablewithsimpsons.html', 
+        title: 'teachablewithsimpsons' 
+    }, 
+    {
+        src: "../assets/projects/podlahy-sucharda/services.webp", 
+        alt: 'podlahysucharda', 
+        link: '/projects/podlahy-sucharda.html', 
+        title: 'Podlahy Sucharda'
+    }, 
+    {
+        src: "../assets/projects/sh-eden/hero_long.webp", 
+        alt: 'sheden', 
+        link: '/projects/sh-eden.html', 
+        title: 'SH Eden'
+    }
+]
+
+// container in header
+const container = document.createElement('div')
+container.className = "projects"
+
+const wrapper = document.createElement('div')
+wrapper.className = "row flex-wrap align-items-center justify-content-start"
+container.appendChild(wrapper)
+
+// loop 
+projects.forEach((project, index) => {
+
+    if (count && index >= count) return false 
+
+    const projectsContainer = document.createElement('div')
+    projectsContainer.className = "p-3 col-lg-6 col-12"
+    const paddingContainer = document.createElement('div')
+    paddingContainer.className = "project"
+
+    const link = document.createElement('a')
+    link.setAttribute('href', project.link)
+
+    const imgWrapper = document.createElement('div')
+    imgWrapper.className = "imgWrapper"
+    imgWrapper.setAttribute('data-title', project.title)
+
+    const img = document.createElement('img')
+    img.setAttribute('src', project.src)
+    img.setAttribute('alt', project.alt)
+    img.setAttribute('width', 500)
+    img.setAttribute('height', 500)
+
+    imgWrapper.appendChild(img)
+    link.appendChild(imgWrapper)
+    paddingContainer.appendChild(link)
+    projectsContainer.appendChild(paddingContainer)
+    wrapper.appendChild(projectsContainer)
+})
+
+
+// Append the container to the shadow root
+custom_projects.appendChild(container)
